8a220bd31f34d6c929b016c18a3cfcfd571699a0
erendgrmnc <erendegirmenci.354@gmail.com>
2022-07-27 02:50:28 +0300

    feat(line): added line object
    # Line object for creating connections between bezier nodes

---
+++ Assets/Scripts/LineController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineController : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private Transform[] points;
+
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        AlignLinePosition();
+    }
+
+    public void SetUpLine(Transform[] points)
+    {
+        lineRenderer.positionCount = points.Length;
+        this.points = points;
+    }
+
+    public void AlignLinePosition()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            lineRenderer.SetPosition(i, points[i].transform.position);
+        }
+
+        var bezierManager = GameObject.FindGameObjectWithTag("BezierManager").GetComponent<BezierManager>();
+        bezierManager.HandleBezierCurve();
+    }
+}
