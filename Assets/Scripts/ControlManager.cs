d2168544197f6760a469c0f135c01ecab90870d4
erendgrmnc <erendegirmenci.354@gmail.com>
2022-07-27 02:47:16 +0300

    feat(control): added mouse events
    # Added ability to move nodes
    
    # Added ability to spawn new bezier nodes

--- Assets/Scripts/ControlManager.cs
+++ Assets/Scripts/ControlManager.cs
@@ -7,6 +7,14 @@ public class ControlManager : MonoBehaviour
     [SerializeField]
     private GameObject bezierNode;
 
+    private BezierManager bezierManager;
+
+    private GameObject bezierNodes;
+
+    private GameObject selectedNode = null;
+    private Vector3 offset = Vector3.zero;
+    private Vector3 mousePosition;
+
     void Awake()
     {
         if (!bezierNode)
@@ -20,28 +28,76 @@ public class ControlManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InitBezierNodesParent();
+        bezierManager = GameObject.FindGameObjectWithTag("BezierManager").GetComponent<BezierManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
         CheckLeftMouseInput();
+        CheckForRightMouseBtnInput();
+    }
+
+    void InitBezierNodesParent()
+    {
+        bezierNodes = new GameObject("BezierNodes");
     }
 
     void CheckLeftMouseInput()
     {
         if (Input.GetMouseButtonDown(0))
+        { 
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+
+            if (targetObject && targetObject.tag == "BezierCurveNode")
+            {
+                selectedNode = targetObject.transform.gameObject;
+                offset = selectedNode.transform.position - mousePosition;
+            }
+        }
+
+        if (selectedNode != null)
         {
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition.z = Camera.main.nearClipPlane;
-            var spawnPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            OnLeftMouseButtonClick(spawnPosition);
+            MoveNode();
+        }
+
+    }
+
+    void CheckForRightMouseBtnInput()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            var isNewNodeSpawnable = bezierManager.IsNodeSpawnable();
+
+            if (isNewNodeSpawnable)
+            {
+                Vector3 mousePosition = Input.mousePosition;
+                mousePosition.z = Camera.main.nearClipPlane;
+                var spawnPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+                SpawnBezierNode(spawnPosition);
+            }
         }
     }
 
-    void OnLeftMouseButtonClick(Vector3 spawnPosition)
+    //TODO: Move to BezierManger (Violation Of Class Responsibility)
+    void SpawnBezierNode(Vector3 spawnPosition)
     {
-        Instantiate(bezierNode, spawnPosition, gameObject.transform.rotation);
+        var newBezierNode = Instantiate(bezierNode, spawnPosition, gameObject.transform.rotation);
+        newBezierNode.transform.parent = bezierNodes.transform;
+        bezierManager.AddNode(newBezierNode);
+    }
+
+    void MoveNode()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        selectedNode.transform.position = mousePosition + offset;
+     
+        if (Input.GetMouseButtonUp(0) && selectedNode)
+        {
+            selectedNode = null;
+        }
     }
 }
