b763c85a25a6ae3ab3b446bbced30d9d9e50e34b
erendgrmnc <erendegirmenci.354@gmail.com>
2022-07-27 22:14:45 +0300

    feat(constants): added constants class script

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Constants
+{
+    public static class Tags
+    {
+        public static string PathFinder = "PathFinder";
+        public static string BezierNode = "BezierCurveNode";
+        public static string BezierManager = "BezierManager";
+    }
+
+    public static class UITexts
+    {
+        public static string ToggleSpaceShipButtonActiveText = "Disable Space Ship";
+        public static string ToggleSpaceShipButtonDeactivatedText = "Spawn Space Ship";
+    }
+}
