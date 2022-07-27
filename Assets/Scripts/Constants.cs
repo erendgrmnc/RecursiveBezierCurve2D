87b6f5c3504a23b8e336cb07bb107e23a5595054
erendgrmnc <erendegirmenci.354@gmail.com>
2022-07-28 00:39:53 +0300

    feat(constants): added GameObjectsNames class
    # Added new tags and UITexts

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 namespace Constants
 {
+    public static class GameObjectNames
+    {
+        public static string BezierNodes = "BezierNodes";
+        public static string LinesBetweenBezierNodes = "LinesBetweenBezierNodes";
+    }
+
     public static class Tags
     {
         public static string PathFinder = "PathFinder";
         public static string BezierNode = "BezierCurveNode";
         public static string BezierManager = "BezierManager";
+        public static string Line = "Line";
+        public static string SceneManager = "SceneManager";
     }
 
     public static class UITexts
     {
         public static string ToggleSpaceShipButtonActiveText = "Disable Space Ship";
         public static string ToggleSpaceShipButtonDeactivatedText = "Spawn Space Ship";
+        public static string ToggleLineBetweenNodesButtonActiveText = "Hide Lines Between Nodes";
+        public static string ToggleLineBetweenNodesButtonDeactivatedText = "Show Lines Between Nodes";
     }
 }
